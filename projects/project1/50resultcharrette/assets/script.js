18b1e345eada182d01350be0b5a1fc2ecd03495e
beccakao <rkao@cca.edu>
2017-09-10 02:24:50 -0700

    I don't even know man

--- projects/project1/50resultcharrette/assets/script.js
+++ projects/project1/50resultcharrette/assets/script.js
@@ -1,25 +1,20 @@
-// $(document).ready(function(){
-	
-// 	$('#button-4').click(function() {
-// 		$('#button-3').animate({
-// 			opacity: 0.4,
-// 			marginLeft: '0.6in',
-// 			fontSize: '24px',
-// 			borderWidth: '10px'
-// 		}, 1000 );
-// 	});
-
-// 	$('#button-4').mouseover(function() {
-// 		$('.item').animate({
-// 			marginLeft: '1in'
-// 		}, 1000 );
-// 	});
 // Back to top
 	function topFunction() {
     	document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     	document.documentElement.scrollTop = 0; // For IE and Firefox
 	}
-	
+// Play sound
+	function playSound() {
+          var sound = document.getElementById("audio");
+          sound.play();
+    }
+// Click counter
+    var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+
 $(document).ready(function(){
 	// Make text larger
 	$('#button-1').click(function() {
@@ -80,7 +75,7 @@ $(document).ready(function(){
 		$(this).css("vertical-align","middle")
 	});
     // Round corners
-    $("#button-9").click(function () {
+    $('#button-9').click(function () {
 		  $(this).css({
 		    borderTopLeftRadius: 0, 
 		    borderTopRightRadius: 0, 
@@ -112,13 +107,113 @@ $(document).ready(function(){
 		   	fontSize: '333px',
 		  }, 1000); 
 	});
-	// Make text larger
+	// Make text larger with doubleclick
 	$('#button-11').dblclick(function() {
 		$(this).animate({
 			fontSize: '333px',
 		}, 1000 );
 	});
-	
+	// Make text larger with hover
+	$('#button-12').mouseover(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 1000 );
+	});
+	// Change cursor
+	$('#button-13').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 1000 );
+		$(this).css('cursor','crosshair');
+	});
+	// Move left & right
+    $( "#right" ).click(function() {
+  		$( ".container" ).animate({ "left": "+=100px" }, "slow" );
+	});
+	$( "#left" ).click(function(){
+  		$( ".container" ).animate({ "left": "-=100px" }, "slow" );
+	});
+	// Move up & down
+    $( "#up" ).click(function() {
+  		$( ".container" ).animate({ "top": "-=100px" }, "slow" );
+	});
+	$( "#down" ).click(function(){
+  		$( ".container" ).animate({ "top": "+=100px" }, "slow" );
+	});
+	// Lights on
+	$('#button-16').click(function() {
+		$('body').css('background-color', 'white')
+	});
+	// Lights off
+	$('#button-17').click(function() {
+		$('body').css('background-color', 'black')
+	});
+	// Hightlight
+	$('#button-18').click(function() {
+		$('h1').css('background-color', 'yellow')
+		$('p').css('background-color', 'yellow')
+		$('button').css('background-color', 'yellow')
+	});
+	// Deselect
+	$('#button-19').click(function() {
+		$('h1').css('background-color', 'transparent')
+		$('p').css('background-color', 'transparent')
+		$('button').css('background-color', 'transparent')
+	});
+	// Change cursor
+	$('#button-23').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 20000 );
+		$(this).css('cursor','wait');
+	});
+	// Draggable
+	$(function() {
+	    $("#button-24").draggable();
+		});
+	});
+	// Bounce
+	$('#button-25').click(function(){
+    	$("#button-25").effect("bounce", { times:3 }, 2000);
+	})
+	// Zoom out
+	$('#button-26').click(function(){
+    	$("#button-26").effect("scale", {percent: 50}, 2000);
+	});
+	// Zoom in
+	$('#button-27').click(function(){
+    	$("#button-27").effect("scale", {percent: 200}, 2000);
+	});
+	// Shake
+	$('#button-28').click(function(){
+    	$("#button-28").effect("shake", { times:3 }, 500);
+	})
+	// Fold
+	$('#button-29').click(function(){
+    	$("#button-29").effect("fold", {horizFirst: true}, 2000);
+	})
+	// Explode
+	$('#button-35').click(function(){
+    	$("#button-35").css("background-color","yellow")
+    	$("#button-35").effect("explode", 3000);
+	});
+	// Clip
+	$('#button-40').click(function(){
+		$(this).animate({
+			fontSize: '333px',
+		}, 1000 );
+    	$("#button-40").effect("clip", 1000);
+	});
+	// Puff
+	$('#button-41').click(function(){
+		$("#button-41").css("background-color","red")
+    	$("#button-41").effect("puff", 2000);
+	});
+	// Pulsate
+	$('#button-44').click(function(){
+		$("#button-44").css("background-color","yellow")
+    	$("#button-44").effect("pulsate", 500);
+    });
 	// Hide
 	$('#button-45').click(function() {
 		$(this).hide()
