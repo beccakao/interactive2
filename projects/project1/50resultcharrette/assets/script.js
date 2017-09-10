41fc573b6510141b1b8b58e8e8624023fc631961
beccakao <rkao@cca.edu>
2017-09-09 17:14:38 -0700

    1-8, 48-50

--- projects/project1/50resultcharrette/assets/script.js
+++ projects/project1/50resultcharrette/assets/script.js
@@ -15,4 +15,92 @@
 // 		}, 1000 );
 // 	});
 
-// });
+$(document).ready(function(){
+	// Make text larger
+	$('#button-1').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 1000 );
+	});
+	// Change background color
+	$('#button-2').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 1000 );
+		$(this).css("background-color","red")
+	});
+	// Change background opacity
+	$('#button-3').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+			opacity: 0.5,
+		}, 1000 );
+		$(this).css("background-color","red")
+	});
+	// Change height
+	$('#button-4').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+			height: '50%',
+		}, 1000 );
+	});
+	// Change width
+	$('#button-5').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+			width: '9%',
+		}, 1000 );
+	});
+	// "Fade out"
+	$('#button-6').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+			opacity:0,
+		}, 1000 );
+	}); 
+	// Pop up
+	$('#button-7').click(function() {
+		$(this).animate({
+			fontSize: '333px',
+		}, 3000 );
+		alert("7 IS A LUCKY NUMBER.");
+	});
+	// Make new text appear
+	$('#button-8').click(function() {
+		$(this).text(":)");
+		$(this).css("font-family","helvetica, arial, sans-serif")
+		$(this).css("font-weight","bold")
+		$(this).css("font-size","12em")
+		$(this).css("text-align","center")
+		$(this).css("vertical-align","middle")
+	});
+
+    $('#button-9').click(function() {
+	});
+
+	// Hide
+	$('#button-48').click(function() {
+		$(this).hide()
+	});
+	// New tab
+	$('#button-49').click(function closeWin() {
+		$( this ).attr( 'target', '_blank' );
+		window.location = $(this).find("a").attr("href");
+        return false;
+	});
+	// Close tab
+	$('#button-50').click(function closeWin() {
+    	window.top.close();
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
