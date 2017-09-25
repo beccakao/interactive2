676f287a245e5eef24ae11311b67f2ada5e2473f
beccakao <rkao@cca.edu>
2017-09-25 12:03:38 -0700

    Project1

---
+++ projects/project1/phonetics/assets/script.js
@@ -0,0 +1,303 @@
+$(document).ready(function(){
+
+  	$("#a").hover(function(){
+  			$("#01a")[0].pause();
+  			$("#01a")[0].currentTime = 0;
+  			$("#01a")[0].play();
+  			});
+ 
+ 		$("#s").hover(function(){
+  			$("#02s")[0].pause();
+  			$("#02s")[0].currentTime = 0;
+  			$("#02s")[0].play();
+  			});
+
+ 		$("#t").hover(function(){
+  			$("#03t")[0].pause();
+  			$("#03t")[0].currentTime = 0;
+  			$("#03t")[0].play();
+  			});
+
+ 		$("#i").hover(function(){
+  			$("#04i")[0].pause();
+  			$("#04i")[0].currentTime = 0;
+  			$("#04i")[0].play();
+  			});
+
+ 		$("#p").hover(function(){
+  			$("#05p")[0].pause();
+  			$("#05p")[0].currentTime = 0;
+  			$("#05p")[0].play();
+  			});
+
+ 		$("#k").hover(function(){
+  			$("#06k")[0].pause();
+  			$("#06k")[0].currentTime = 0;
+  			$("#06k")[0].play();
+  			});
+
+ 		$("#e").hover(function(){
+  			$("#07e")[0].pause();
+  			$("#07e")[0].currentTime = 0;
+  			$("#07e")[0].play();
+  			});
+
+    $("#n").hover(function(){
+        $("#08n")[0].pause();
+        $("#08n")[0].currentTime = 0;
+        $("#08n")[0].play();
+        });
+
+    $("#h").hover(function(){
+        $("#09h")[0].pause();
+        $("#09h")[0].currentTime = 0;
+        $("#09h")[0].play();
+        });
+    
+    $("#r").hover(function(){
+        $("#10r")[0].pause();
+        $("#10r")[0].currentTime = 0;
+        $("#10r")[0].play();
+        });
+
+    $("#m").hover(function(){
+        $("#11m")[0].pause();
+        $("#11m")[0].currentTime = 0;
+        $("#11m")[0].play();
+        });
+
+    $("#d").hover(function(){
+        $("#12d")[0].pause();
+        $("#12d")[0].currentTime = 0;
+        $("#12d")[0].play();
+        });
+
+    $("#g").hover(function(){
+        $("#13g")[0].pause();
+        $("#13g")[0].currentTime = 0;
+        $("#13g")[0].play();
+        });
+
+    $("#o").hover(function(){
+        $("#14o")[0].pause();
+        $("#14o")[0].currentTime = 0;
+        $("#14o")[0].play();
+        });
+
+    $("#a2").hover(function(){
+        $("#15a")[0].pause();
+        $("#15a")[0].currentTime = 0;
+        $("#15a")[0].play();
+        });
+
+    $("#l").hover(function(){
+        $("#16l")[0].pause();
+        $("#16l")[0].currentTime = 0;
+        $("#16l")[0].play();
+        });
+
+    $("#f").hover(function(){
+        $("#17f")[0].pause();
+        $("#17f")[0].currentTime = 0;
+        $("#17f")[0].play();
+        });
+
+    $("#b").hover(function(){
+        $("#18b")[0].pause();
+        $("#18b")[0].currentTime = 0;
+        $("#18b")[0].play();
+        });
+
+    $("#a3").hover(function(){
+        $("#19a")[0].pause();
+        $("#19a")[0].currentTime = 0;
+        $("#19a")[0].play();
+        });
+
+    $("#j").hover(function(){
+        $("#20j")[0].pause();
+        $("#20j")[0].currentTime = 0;
+        $("#20j")[0].play();
+        });
+
+
+    $("#o2").hover(function(){
+        $("#21o")[0].pause();
+        $("#21o")[0].currentTime = 0;
+        $("#21o")[0].play();
+        });
+
+    $("#i2").hover(function(){
+        $("#22i")[0].pause();
+        $("#22i")[0].currentTime = 0;
+        $("#22i")[0].play();
+        });
+
+    $("#e2").hover(function(){
+        $("#23e")[0].pause();
+        $("#23e")[0].currentTime = 0;
+        $("#23e")[0].play();
+        });
+
+    $("#o3").hover(function(){
+        $("#24o")[0].pause();
+        $("#24o")[0].currentTime = 0;
+        $("#24o")[0].play();
+        });
+
+    $("#z").hover(function(){
+        $("#25z")[0].pause();
+        $("#25z")[0].currentTime = 0;
+        $("#25z")[0].play();
+        });
+
+    $("#w").hover(function(){
+        $("#26w")[0].pause();
+        $("#26w")[0].currentTime = 0;
+        $("#26w")[0].play();
+        });
+
+    $("#n2").hover(function(){
+        $("#27n")[0].pause();
+        $("#27n")[0].currentTime = 0;
+        $("#27n")[0].play();
+        });
+
+    $("#v").hover(function(){
+        $("#28v")[0].pause();
+        $("#28v")[0].currentTime = 0;
+        $("#28v")[0].play();
+        });
+
+    $("#u").hover(function(){
+        $("#29u")[0].pause();
+        $("#29u")[0].currentTime = 0;
+        $("#29u")[0].play();
+        });
+
+    $("#u2").hover(function(){
+        $("#30u")[0].pause();
+        $("#30u")[0].currentTime = 0;
+        $("#30u")[0].play();
+        });
+  
+    $("#y").hover(function(){
+        $("#31y")[0].pause();
+        $("#31y")[0].currentTime = 0;
+        $("#31y")[0].play();
+        });
+  
+    $("#x").hover(function(){
+        $("#32x")[0].pause();
+        $("#32x")[0].currentTime = 0;
+        $("#32x")[0].play();
+        });
+  
+    $("#ch").hover(function(){
+        $("#33ch")[0].pause();
+        $("#33ch")[0].currentTime = 0;
+        $("#33ch")[0].play();
+        });
+  
+    $("#sh").hover(function(){
+        $("#34sh")[0].pause();
+        $("#34sh")[0].currentTime = 0;
+        $("#34sh")[0].play();
+        });
+  
+    $("#th").hover(function(){
+        $("#35th")[0].pause();
+        $("#35th")[0].currentTime = 0;
+        $("#35th")[0].play();
+        });
+  
+    $("#th").hover(function(){
+        $("#36th")[0].pause();
+        $("#36th")[0].currentTime = 0;
+        $("#36th")[0].play();
+        });
+  
+    $("#q").hover(function(){
+        $("#37q")[0].pause();
+        $("#37q")[0].currentTime = 0;
+        $("#37q")[0].play();
+        });
+
+    $("#ow").hover(function(){
+        $("#38ow")[0].pause();
+        $("#38ow")[0].currentTime = 0;
+        $("#38ow")[0].play();
+        });
+
+    $("#oi").hover(function(){
+        $("#39oi")[0].pause();
+        $("#39oi")[0].currentTime = 0;
+        $("#39oi")[0].play();
+        });
+
+    $("#u3").hover(function(){
+        $("#40u")[0].pause();
+        $("#40u")[0].currentTime = 0;
+        $("#40u")[0].play();
+        });
+
+    $("#er").hover(function(){
+        $("#41er")[0].pause();
+        $("#41er")[0].currentTime = 0;
+        $("#41er")[0].play();
+        });
+
+    $("#ar").hover(function(){
+        $("#42ar")[0].pause();
+        $("#42ar")[0].currentTime = 0;
+        $("#42ar")[0].play();
+        });
+
+    $("#d2").hover(function(){
+        $("#43d")[0].pause();
+        $("#43d")[0].currentTime = 0;
+        $("#43d")[0].play();
+        });
+
+    $("#h2").hover(function(){
+        $("#44h")[0].pause();
+        $("#44h")[0].currentTime = 0;
+        $("#44h")[0].play();
+        });
+
+    $("#p2").hover(function(){
+        $("#45p")[0].pause();
+        $("#45p")[0].currentTime = 0;
+        $("#45p")[0].play();
+        });
+
+    $("#n3").hover(function(){
+        $("#46n")[0].pause();
+        $("#46n")[0].currentTime = 0;
+        $("#46n")[0].play();
+        });
+
+    $("#m2").hover(function(){
+        $("#47m")[0].pause();
+        $("#47m")[0].currentTime = 0;
+        $("#47m")[0].play();
+        });
+
+    $("#k2").hover(function(){
+        $("#48k")[0].pause();
+        $("#48k")[0].currentTime = 0;
+        $("#48k")[0].play();
+        });
+
+    $("#e3").hover(function(){
+        $("#493")[0].pause();
+        $("#493")[0].currentTime = 0;
+        $("#493")[0].play();
+        });
+
+    $("#r2").hover(function(){
+        $("#50r")[0].pause();
+        $("#50r")[0].currentTime = 0;
+        $("#50r")[0].play();
+        });
+});
