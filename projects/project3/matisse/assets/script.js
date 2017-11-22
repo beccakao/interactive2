d59c11c83487574c56d69c186e30366ad09a0062
beccakao <rkao@cca.edu>
2017-11-22 08:10:02 -0800

    Start exhibition microsite

---
+++ projects/project3/matisse/assets/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(function() {
+        $( "#draggable1" ).draggable();
+    });
+
+    $(function() {
+        $( "#draggable2" ).draggable();
+    });
+
+    $(function() {
+        $( "#draggable3" ).draggable();
+    });
+
+    $(function() {
+        $( "#draggable4" ).draggable();
+    });
+
+    $(function() {
+        $( "#draggable5" ).draggable();
+    });
+
+
+});
